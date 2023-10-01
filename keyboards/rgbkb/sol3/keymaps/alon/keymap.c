128245ce3cdd5a5236fea1d405ede0e6ed405a9d
Alon Tzarafi <alontzarafi@gmail.com>
2023-10-01 14:52:52 +0300

    Now keymap is working properly

--- keyboards/rgbkb/sol3/keymaps/alon/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/alon/keymap.c
@@ -60,8 +60,8 @@ enum sol_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
-        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                  KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    MU_TOGG,                  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MU_NEXT,                  KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         FN,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN,                  KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCBR,                  KC_RCBR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL,  KC_LGUI, KC_LALT, MIRROR,  FN,      KC_SPC,  KC_PGDN, CTL_DEL, KC_ENT, KC_PGUP, KC_ENT,  FN,      KC_MINS, KC_EQL,  KC_RGUI, KC_RCTL,
@@ -80,11 +80,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_FN] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-        _______, KC_HOME, KC_UP,   KC_END,  KC_F11,  KC_F12,  _______,                   _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PSCR, _______,
+        KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                   KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
+        _______, KC_PGUP, KC_UP,   KC_PGDN, KC_F11,  KC_F12,  _______,                   _______, _______, KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, _______,
         KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  _______, _______,                   _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  _______,
-        _______, AU_TOGG, MU_TOGG, MU_NEXT, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, RGB,     _______, KC_ENT,  _______, KC_INS,  _______, QK_BOOT, _______, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,
+        _______, KC_MPRV, KC_MNXT, KC_MPLY, KC_VOLD, KC_VOLU, _______,                   _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_APP,  _______, RGB,     _______, KC_ENT,  _______, KC_INS,  _______, QK_BOOT, _______, _______, KC_VOLD, KC_VOLU, _______, _______,
 
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
